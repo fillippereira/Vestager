0ddd0fe26bfd503a6573d752c7a45384a1eaf821
Fillip Pereira <fillip.pereira@gmail.com>
2021-01-11 01:38:42 -0300

    First commit

---
+++ Vestager.Domain/Cliente.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vestager.Domain
+{
+    public class Cliente
+    {
+    }
+}
---
+++ Vestager.Infra/Data/Context.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Vestager.Domain;
+
+namespace Vestager.Infra.Data
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<DbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+
+        public virtual DbSet<Cliente> Clientes { get; set; }
+        public virtual DbSet<Locacao> Locacoes { get; set; }
+        public virtual DbSet<Prova> Provas { get; set; }
+        public virtual DbSet<Vestido> Vestidos { get; set; }
+
+    }
+}
